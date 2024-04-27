df9b8803044a2d0f5357de4ae7017df8198a3875
Luis Manuel <lmanuel@capi.lat>
2024-04-27 15:27:04 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("htmlcss");
+        habilidades[1].classList.add("javascript");
+        habilidades[2].classList.add("vuejs");
+        habilidades[3].classList.add("php");
+        habilidades[4].classList.add("laravel");
+        habilidades[5].classList.add("git");
+        habilidades[6].classList.add("mysql");
+        habilidades[7].classList.add("mssql");
+        habilidades[8].classList.add("docker");
+        habilidades[9].classList.add("oci");
+        habilidades[10].classList.add("azure");
+        habilidades[11].classList.add("servidores");   
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
